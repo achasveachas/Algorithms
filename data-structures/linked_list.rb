a3a3d1a293a6cae2d113b5282b2f2929160556bb
Yechiel K <chilikkal@gmail.com>
2017-03-30 15:38:50 -0400

    added some methods to linked_list.rb

--- data-structures/linked_list.rb
+++ data-structures/linked_list.rb
@@ -14,9 +14,21 @@ end
 class LinkedList
   attr_accessor :head
 
+  #class methods
+
   def initialize(node)
     @head = node
   end
+
+  def self.seed
+    ll = self.new(Node.new(1))
+    10.times do |i|
+      ll.add(Node.new(i + 2))
+    end
+    ll
+  end
+
+  #methods to get information about the list
 
   def tail
     current_node = self.head
@@ -28,24 +40,69 @@ class LinkedList
     current_node
   end
 
+  def length
+    length = 0
+    self.each{ length += 1}
+    length
+  end
+
+  def find(value)
+    found = false
+    self.each do |node|
+      found = true if node.value == value
+    end
+    found
+  end
+
+  #methods to add to the list
+
   def add_to_head(node)
-    node.next = self.head
-    self.head = node
-    self
+    if self.find(node.value)
+      puts "Sorry, a node with value #{node.value} already exists"
+    else
+      node.next = self.head
+      self.head = node
+      puts "Node #{node.value} was added to the head of the list"
+    end
+    self.to_str
   end
 
   def add(node)
-    self.tail.next = node
-    self
+    if self.find(node.value)
+      puts "Sorry, a node with value #{node.value} already exists"
+    else
+      self.tail.next = node
+      puts "Node #{node.value} was added to the list"
+    end
+    self.to_str
   end
 
-  def length
-    length = 0
-    self.each do
-      length += 1
+  #methods to remove from list
+
+  def remove(value)
+    if self.head.value == value
+      self.head = self.head.next
+      puts "Removed node with value #{value}"
+      return self.to_str
     end
-    length
+
+    before_remove = nil
+    self.each do |node|
+      if node.next && node.next.value == value
+        before_remove = node
+      end
+    end
+    if !before_remove
+      puts "Couldn't find a node with value #{value}"
+    else
+      to_remove = before_remove.next
+      before_remove.next = to_remove.next
+      puts "Node with value #{to_remove.value} has been removed"
+    end
+    self.to_str
   end
+
+  #iterators
 
   def each(&block)
       current_node = self.head
@@ -57,7 +114,7 @@ class LinkedList
 
   #link_list.to_str()
   #Prints a string representation of link_list
-  def to_str()
+  def to_str
       self.each do |node|
           puts node.to_str()
       end
