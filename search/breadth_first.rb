2eddc37ebc5fc57c9b5181ded670acd0a0ce7838
Yechiel K <chilikkal@gmail.com>
2017-04-05 20:39:55 -0400

    added breadth_first.rb

---
+++ search/breadth_first.rb
@@ -0,0 +1,7 @@
+# Breadth First is a search algorithm for searching through nested structures.
+# It works by searching through the "breadth" of the top layer before digging to the "depth" of the next layer and so on...
+# By contrast Depth First digs down into each element as it finds it before moving on to the next one.
+
+def breadth_first(array, value)
+
+end
