05c975b2adf02020e576f10ed2974c5447f42c61
Yechiel K <chilikkal@gmail.com>
2017-04-07 00:24:51 -0400

    added selection_sort.rb

---
+++ sort/selection_sort.rb
@@ -0,0 +1,5 @@
+# Selection Sort is an algorithm that sorts an unordered array by finding the lowest value and swapping it for index 0, then finding the second lowest value and swapping it with index 1 etc. until the whole array is sorted.
+
+def selection_sort(array)
+  
+end
