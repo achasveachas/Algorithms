bdf2345c31ff12c3a821ecf56c5d1ce10ba4966b
Yechiel K <chilikkal@gmail.com>
2017-04-23 00:43:53 -0400

    implememtned inefficient sock sorting method

--- sort/sock_sort.rb
+++ sort/sock_sort.rb
@@ -1,9 +1,19 @@
 # Based on the scenario in the book Bad Choices by Ali Almossawi
 
-def inefficient_sort(array)
-
+# The methods below deal with sock piles, in real life they would be known as arrays
+def inefficient_sort(sock_pile)
+  sorted_socks = []
+   until sock_pile == sock_pile.uniq
+     match = Array(sock_pile.pop)
+     sock2 = sock_pile.find do |sock|
+       sock == match[0]
+     end
+     match << sock2
+     sorted_socks << match
+   end
+   sorted_socks
 end
 
-def efficient_sort(array)
+def efficient_sort(sock_pile)
 
 end
